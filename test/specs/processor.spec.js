0acb42b5ef4f43232009b68843c83c1292943a6d
Chris Bigelow <seebigs@gmail.com>
2017-01-13 00:12:26 -0500

    CB: update test

--- test/specs/processor.spec.js
+++ test/specs/processor.spec.js
@@ -1,21 +1,22 @@
 
-var processorLess = require('../../index.js');
-var builtIn = {
-    css: {
+var lessProcessor = require('../../index.js')({});
+
+function getProcessor (ext) {
+    return {
         processor: function (file) {
             return file.contents + '(via css)';
         },
         autoInject: function () {
 
         }
-    }
-};
+    };
+}
 
 describe('processor', function () {
 
     describe('does the job', function (expect) {
-        var processor = processorLess('less', builtIn);
-        var output = processor.processor({ contents: '.foo{ color:blue; .bar{ color:red; } }' });
+        var p = lessProcessor(getProcessor);
+        var output = p.processor({ contents: '.foo{ color:blue; .bar{ color:red; } }' });
         expect(output).toBe('.foo {\n  color: blue;\n}\n.foo .bar {\n  color: red;\n}\n(via css)');
     });
 
